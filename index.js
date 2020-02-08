986bdf24d5d60c25fb53b7e64430c52ec567bc06
whichxjy <whichxjy@gmail.com>
2020-02-09 00:18:03 +0800

    Init download method

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const client = require('./lib/core/client')
-const tracker = require('./lib/core/tracker')
 
 const myPath = './lubuntu-18.04.3-desktop-amd64.iso.torrent'
 
-client.openTorrentFile(myPath).then(async (tf) => {
-  await tracker.requestPeers(tf, '123', 45644)
-})
+;(async () => {
+  await client.openTorrentFile(myPath).then(client.downloadToFile, 'target_path')
+})()
