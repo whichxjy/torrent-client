27d40d67ec7d6e7eb5368dcbac78f5021e51de7d
whichxjy <whichxjy@gmail.com>
2020-02-11 07:03:13 +0800

    Add tests for handshake

---
+++ test/handshake.test.js
@@ -0,0 +1,40 @@
+const assert = require('assert').strict
+const { describe, it } = require('mocha')
+const handshake = require('../lib/core/handshake')
+
+describe('handshake', () => {
+  describe('#serialize()', () => {
+    const tests = [
+      {
+        result: handshake.serialize(new handshake.Handshake(
+          '',
+          Buffer.from([134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116]),
+          Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+        )),
+        expect: Buffer.from(Buffer.from([0, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+      },
+      {
+        result: handshake.serialize(new handshake.Handshake(
+          'Hello',
+          Buffer.from([134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116]),
+          Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+        )),
+        expect: Buffer.from(Buffer.from([5, 72, 101, 108, 108, 111, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+      },
+      {
+        result: handshake.serialize(new handshake.Handshake(
+          'BitTorrent Protocol',
+          Buffer.from([134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116]),
+          Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+        )),
+        expect: Buffer.from(Buffer.from([19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 80, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+      }
+    ]
+
+    tests.forEach((test, i) => {
+      it('test ' + (i + 1), () => {
+        assert.deepEqual(test.result, test.expect)
+      })
+    })
+  })
+})
