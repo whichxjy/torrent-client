c6b69e03c40c2cbbdf33f5eeab055feff9ec0974
whichxjy <whichxjy@gmail.com>
2020-02-11 01:19:19 +0800

    Update tests for message

--- test/message.test.js
+++ test/message.test.js
@@ -4,22 +4,33 @@ const message = require('../lib/core/message')
 
 describe('message', () => {
   describe('#serializeMessage()', () => {
-    it('test 1', function () {
-      const result = message.serializeMessage({
-        id: message.msgChoke,
-        payload: new Uint8Array([])
-      })
-      const expect = Buffer.from(new Uint8Array([0, 0, 0, 1, 0]))
-      assert.deepEqual(result, expect)
-    })
+    const tests = [
+      {
+        result: message.serializeMessage({
+          id: message.msgChoke
+        }),
+        expect: Buffer.from(new Uint8Array([0, 0, 0, 1, 0]))
+      },
+      {
+        result: message.serializeMessage({
+          id: message.msgChoke,
+          payload: new Uint8Array([])
+        }),
+        expect: Buffer.from(new Uint8Array([0, 0, 0, 1, 0]))
+      },
+      {
+        result: message.serializeMessage({
+          id: message.msgHave,
+          payload: new Uint8Array([1, 2, 3, 4, 5])
+        }),
+        expect: Buffer.from(new Uint8Array([0, 0, 0, 6, 4, 1, 2, 3, 4, 5]))
+      }
+    ]
 
-    it('test 2', function () {
-      const result = message.serializeMessage({
-        id: message.msgHave,
-        payload: new Uint8Array([1, 2, 3, 4, 5])
+    tests.forEach((test, i) => {
+      it('test ' + (i + 1), () => {
+        assert.deepEqual(test.result, test.expect)
       })
-      const expect = Buffer.from(new Uint8Array([0, 0, 0, 6, 4, 1, 2, 3, 4, 5]))
-      assert.deepEqual(result, expect)
     })
   })
 })
