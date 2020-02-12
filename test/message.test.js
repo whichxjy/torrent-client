429c0f484640e7bb1d3eb406cd1cf43ae1dd20a0
whichxjy <whichxjy@gmail.com>
2020-02-12 15:55:21 +0800

    Update tests for message

--- test/message.test.js
+++ test/message.test.js
@@ -25,4 +25,39 @@ describe('message', () => {
       })
     })
   })
+
+  describe('#createRequest()', () => {
+    const tests = [
+      {
+        result: message.createRequest(1, 2, 3),
+        expect: new message.Message(message.REQUEST, Buffer.from([
+          0x00, 0x00, 0x00, 0x01,
+          0x00, 0x00, 0x00, 0x02,
+          0x00, 0x00, 0x00, 0x03
+        ]))
+      },
+      {
+        result: message.createRequest(123, 456, 789),
+        expect: new message.Message(message.REQUEST, Buffer.from([
+          0x00, 0x00, 0x00, 0x7B,
+          0x00, 0x00, 0x01, 0xC8,
+          0x00, 0x00, 0x03, 0x15
+        ]))
+      },
+      {
+        result: message.createRequest(1111, 2222, 3333),
+        expect: new message.Message(message.REQUEST, Buffer.from([
+          0x00, 0x00, 0x04, 0x57,
+          0x00, 0x00, 0x08, 0xAE,
+          0x00, 0x00, 0x0D, 0x05
+        ]))
+      }
+    ]
+
+    tests.forEach((test, i) => {
+      it('test ' + (i + 1), () => {
+        assert.deepEqual(test.result, test.expect)
+      })
+    })
+  })
 })
